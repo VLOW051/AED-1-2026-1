f3bbdec1236248c18fcb0aba9e232550e650e2dc
VLOW051 <vitorvelloso4@gmail.com>
2026-05-17 14:13:27 -0300

    implementeção do main interativo

--- exercicio_2/leetCode_1019.c
+++ exercicio_2/leetCode_1019.c
@@ -1,21 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-typedef struct ListNode {
-      int val;
-      struct ListNode *next;
- } Node;
-
-
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
 
 int* nextLargerNodes(struct ListNode* head, int* returnSize);
+struct ListNode* createNode(int val);
+struct ListNode* buildList(int* arr, int size);
+void freeList(struct ListNode* head);
 
+int main() {
+    int inputSize;
 
-int main(){
-  
-    
-  
+    printf("Digite o tamanho do vetor: ");
+    if (scanf("%d", &inputSize) != 1 || inputSize <= 0) {
+        printf("Tamanho invalido.\n");
+        return 1;
+    }
+
+    int* inputData = (int*)malloc(inputSize * sizeof(int));
+    if (inputData == NULL) {
+        printf("Erro ao alocar memoria.\n");
+        return 1;
+    }
+
+    printf("Digite os %d elementos do vetor:\n", inputSize);
+    for (int i = 0; i < inputSize; i++) {
+        printf("Elemento [%d]: ", i);
+        if (scanf("%d", &inputData[i]) != 1) {
+            printf("Entrada invalida.\n");
+            free(inputData);
+            return 1;
+        }
+    }
+
+    struct ListNode* head = buildList(inputData, inputSize);
+
+    int returnSize = 0;
+    int* result = nextLargerNodes(head, &returnSize);
+
+    printf("\nInput:  [");
+    for (int i = 0; i < inputSize; i++) {
+        printf("%d%s", inputData[i], (i == inputSize - 1) ? "" : ", ");
+    }
+    printf("]\n");
+
+    printf("Output: [");
+    for (int i = 0; i < returnSize; i++) {
+        printf("%d%s", result[i], (i == returnSize - 1) ? "" : ", ");
+    }
+    printf("]\n");
+
+    free(inputData);
+    free(result);
+    freeList(head);
+
     return 0;
 }
 
@@ -55,4 +96,31 @@ int* nextLargerNodes(struct ListNode* head, int* returnSize) {
 
     *returnSize = size;
     return result;
+}
+
+struct ListNode* createNode(int val) {
+    struct ListNode* newNode = (struct ListNode*)malloc(sizeof(struct ListNode));
+    newNode->val = val;
+    newNode->next = NULL;
+    return newNode;
+}
+
+struct ListNode* buildList(int* arr, int size) {
+    if (size == 0) return NULL;
+    struct ListNode* head = createNode(arr[0]);
+    struct ListNode* curr = head;
+    for (int i = 1; i < size; i++) {
+        curr->next = createNode(arr[i]);
+        curr = curr->next;
+    }
+    return head;
+}
+
+void freeList(struct ListNode* head) {
+    struct ListNode* tmp;
+    while (head) {
+        tmp = head;
+        head = head->next;
+        free(tmp);
+    }
 }
